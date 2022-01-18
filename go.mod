911342960b20c52040a7e341c35fc4949d1c2533
Koki Fukuda <ko.fu.dev@gmail.com>
2022-01-18 21:48:49 +0900

    Implement WebSocket monitor

--- go.mod
+++ go.mod
@@ -19,6 +19,7 @@ require (
 	github.com/gorilla/context v1.1.1 // indirect
 	github.com/gorilla/securecookie v1.1.1 // indirect
 	github.com/gorilla/sessions v1.2.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/json-iterator/go v1.1.9 // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
--- monitor/monitor.go
+++ monitor/monitor.go
@@ -3,188 +3,98 @@ package monitor
 import (
 	"crypto/tls"
 	"crypto/x509"
-	"encoding/binary"
 	"errors"
-	"io"
+	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"path/filepath"
 	"time"
 
 	"chronoscoper.com/premises/config"
+	"github.com/gorilla/websocket"
 )
 
-const (
-	StatusPayloadVerifyVersion  = byte('v')
-	StatusPayloadSubscribe      = byte('s')
-	StatusPayloadResult         = byte('R')
-	StatusPayloadStatus         = byte('S')
-	StatusPayloadStop           = byte('t')
-	StatusPayloadServerFinished = byte('F')
-)
-
-func readStatus(conn io.Reader) (bool, error) {
-	var buf [4]byte
-	_, err := io.ReadFull(conn, buf[:3])
-	if err != nil {
-		return false, err
-	}
-	if buf[0] != StatusPayloadResult {
-		return false, errors.New("Not a result payload")
-	}
-	dataLength := binary.LittleEndian.Uint16(buf[1:])
-	if dataLength != 1 {
-		return false, errors.New("Invalid length")
-	}
-
-	_, err = io.ReadFull(conn, buf[:1])
-	if err != nil {
-		return false, err
-	}
-
-	return buf[0] == 1, nil
+type statusData struct {
+	Status   string `json:"status"`
+	Shutdown bool   `json:"shutdown"`
+	HasError bool   `json:"hasError"`
 }
 
-const (
-	RequestStopServer = iota
-)
-
-func MonitorServer(config *config.Config, addr string, evCh chan string, reqCh chan int) error {
+func makeTLSConfig(config *config.Config) (*tls.Config, error) {
 	rootCAs := x509.NewCertPool()
 	certFile, err := os.ReadFile(filepath.Join(config.Prefix, "/opt/premises/server.crt"))
 	if err != nil {
-		return err
+		return nil, err
 	}
 	rootCAs.AppendCertsFromPEM(certFile)
 
-	tlsConfig := &tls.Config{
+	return &tls.Config{
 		RootCAs: rootCAs,
+	}, nil
+
+}
+
+func MonitorServer(cfg *config.Config, addr string, evCh chan string) error {
+	tlsConfig, err := makeTLSConfig(cfg)
+	if err != nil {
+		return err
 	}
 
-	go func() {
-		lastConnected := int(time.Now().Unix())
-		evCh <- "Connecting..."
+	dialer := &websocket.Dialer{
+		TLSClientConfig: tlsConfig,
+	}
+	for {
+	newConn:
+		conn, _, err := dialer.Dial("wss://"+addr+"/monitor", http.Header{"X-Auth-Key": []string{cfg.MonitorKey}})
+		if err != nil {
+			log.Println(err)
+			time.Sleep(10 * time.Second)
+			goto newConn
+		}
+		defer conn.Close()
+
 		for {
-			conn, err := tls.Dial("tcp", addr, tlsConfig)
-			if err != nil {
+			var status statusData
+			if err := conn.ReadJSON(&status); err != nil {
 				log.Println(err)
-				evCh <- "Connection failed"
-
-				if int(time.Now().Unix())-lastConnected > int(time.Minute*10) {
-					// No connection for 10 minutes, server's died?
-					close(evCh)
-					break
-				}
-
-				time.Sleep(time.Second * 10)
-				evCh <- "Connecting..."
-				continue
-			}
-			defer conn.Close()
-			lastConnected = int(time.Now().Unix())
-
-			var buf [7]byte
-			buf[0] = StatusPayloadVerifyVersion
-			binary.LittleEndian.PutUint16(buf[1:], uint16(4))
-			binary.LittleEndian.PutUint32(buf[3:], uint32(1))
-
-			if _, err = conn.Write(buf[:]); err != nil {
-				log.Println(err)
-				continue
+				goto newConn
 			}
 
-			ok, err := readStatus(conn)
-			if err != nil {
-				log.Println(err)
-				continue
-			}
-			if !ok {
-				log.Println("Version verification failed")
-				evCh <- "Can't monitor! Version is not match."
-				break
-			}
-			log.Println("Version verified")
+			evCh <- status.Status
 
-			buf[0] = StatusPayloadSubscribe
-			binary.LittleEndian.PutUint16(buf[1:], uint16(len([]byte(config.MonitorKey))))
-			if _, err := conn.Write(buf[0:3]); err != nil {
-				log.Println(err)
-				continue
-			}
-
-			if _, err := conn.Write([]byte(config.MonitorKey)); err != nil {
-				log.Println(err)
-				continue
-			}
-			ok, err = readStatus(conn)
-			if err != nil {
-				log.Println(err)
-				continue
-			}
-			if !ok {
-				log.Println("Event subscription failed")
-				continue
-			}
-			log.Println("Event subscribed successfully")
-
-			inEventChannel := make(chan string)
-			go func() {
-				for {
-					var buf [3]byte
-					if _, err := io.ReadFull(conn, buf[:]); err != nil && err == io.EOF {
-						break
-					} else if err != nil {
-						log.Println(err)
-						continue
-					}
-
-					dataLength := binary.LittleEndian.Uint16(buf[1:])
-
-					if buf[0] == StatusPayloadServerFinished {
-						log.Println("Server finished")
-						close(evCh)
-						close(inEventChannel)
-						break
-					} else if buf[0] == StatusPayloadStatus {
-						data := make([]byte, dataLength)
-						if _, err := io.ReadFull(conn, data); err != nil {
-							log.Println(err)
-							continue
-						}
-
-						inEventChannel <- string(data)
-					} else {
-						log.Println("Payload is not status")
-						buf := make([]byte, dataLength)
-						io.ReadFull(conn, buf)
-						continue
-					}
-				}
-			}()
-
-			for {
-				select {
-				case req := <-reqCh:
-					if req == RequestStopServer {
-						var buf [3]byte
-						buf[0] = StatusPayloadStop
-						if _, err := conn.Write(buf[:]); err != nil {
-							log.Println(err)
-							continue
-						}
-					}
-					break
-
-				case ev, ok := <-inEventChannel:
-					if !ok {
-						goto finish
-					}
-					evCh <- ev
-					break
-				}
+			if status.Shutdown {
+				close(evCh)
+				goto end
 			}
 		}
-	finish:
-	}()
+	}
+end:
+	return nil
+}
+
+func StopServer(cfg *config.Config, addr string) error {
+	tlsConfig, err := makeTLSConfig(cfg)
+	client := http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: tlsConfig,
+		},
+	}
+
+	req, err := http.NewRequest("GET", "https://"+addr+"/stop", nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Add("X-Auth-Key", cfg.MonitorKey)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return errors.New(fmt.Sprintf("StopServer: request failed: %d", resp.StatusCode))
+	}
 	return nil
 }
