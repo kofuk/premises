4026639281109c4cacfe45f33daf5cb61258e691
Koki Fukuda <ko.fu.dev@gmail.com>
2023-09-17 14:37:54 +0900

    Write E2E tests a little for local execution

--- e2e-tests/playwright.config.ts
+++ e2e-tests/playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from '@playwright/test';
+import {defineConfig, devices} from '@playwright/test';
 
 export const TARGET_HOST = process.env.TARGET_HOST || 'http://localhost:8000';
 
@@ -6,41 +6,58 @@ export const TARGET_HOST = process.env.TARGET_HOST || 'http://localhost:8000';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
-  /* Run tests in files in parallel */
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [ ['html', { open: 'never' }] ],
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    testDir: './tests',
+    /* Run tests in files in parallel */
+    fullyParallel: true,
+    /* Fail the build on CI if you accidentally left test.only in the source code. */
+    forbidOnly: !!process.env.CI,
+    /* Retry on CI only */
+    retries: process.env.CI ? 2 : 0,
+    /* Opt out of parallel tests on CI. */
+    workers: 1,
+    /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+    reporter: [['html', {open: 'never'}]],
+    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+    use: {
+        /* Base URL to use in actions like `await page.goto('/')`. */
+        // baseURL: 'http://127.0.0.1:3000',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-  },
-
-  /* Configure projects for major browsers */
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+        /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+        trace: 'on-first-retry'
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    /* Configure projects for major browsers */
+    projects: [
+        {
+            name: 'setup',
+            testMatch: /.*\.setup\.ts/
+        },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-  ],
+        {
+            name: 'chromium',
+            use: {
+                ...devices['Desktop Chrome'],
+                storageState: 'playwright/.auth/user.json'
+            },
+            dependencies: ['setup']
+        },
+
+        {
+            name: 'firefox',
+            use: {
+                ...devices['Desktop Firefox'],
+                storageState: 'playwright/.auth/user.json'
+            },
+            dependencies: ['setup']
+        },
+
+        {
+            name: 'webkit',
+            use: {
+                ...devices['Desktop Safari'],
+                storageState: 'playwright/.auth/user.json'
+            },
+            dependencies: ['setup']
+        }
+    ]
 });
---
+++ e2e-tests/tests/login.setup.ts
@@ -0,0 +1,15 @@
+import {test as setup, expect} from '@playwright/test';
+import {TARGET_HOST} from '../playwright.config';
+
+const authFile = 'playwright/.auth/user.json';
+
+setup('login', async ({page}) => {
+    await page.goto(TARGET_HOST);
+    await page.getByLabel('User').fill('user1');
+    await page.getByLabel('Password').fill('password');
+    await page.getByRole('button', {name: 'Login'}).click();
+
+    await expect(page.getByText('Logout')).toBeVisible();
+
+    await page.context().storageState({path: authFile});
+});
