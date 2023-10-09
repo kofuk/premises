3b61766de774f84b2b99ad2462142a599c3e4a99
Koki Fukuda <ko.fu.dev@gmail.com>
2023-10-09 22:14:57 +0900

    Format files under e2e-tests

--- e2e-tests/playwright.config.ts
+++ e2e-tests/playwright.config.ts
@@ -6,58 +6,58 @@ export const TARGET_HOST = process.env.TARGET_HOST || 'http://localhost:8000';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-    testDir: './tests',
-    /* Run tests in files in parallel */
-    fullyParallel: true,
-    /* Fail the build on CI if you accidentally left test.only in the source code. */
-    forbidOnly: !!process.env.CI,
-    /* Retry on CI only */
-    retries: process.env.CI ? 2 : 0,
-    /* Opt out of parallel tests on CI. */
-    workers: 1,
-    /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: [['html', {open: 'never'}]],
-    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-    use: {
-        /* Base URL to use in actions like `await page.goto('/')`. */
-        // baseURL: 'http://127.0.0.1:3000',
+  testDir: './tests',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: 1,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: [['html', {open: 'never'}]],
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    // baseURL: 'http://127.0.0.1:3000',
 
-        /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-        trace: 'on-first-retry'
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry'
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'setup',
+      testMatch: /.*\.setup\.ts/
     },
 
-    /* Configure projects for major browsers */
-    projects: [
-        {
-            name: 'setup',
-            testMatch: /.*\.setup\.ts/
-        },
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+        storageState: 'playwright/.auth/user.json'
+      },
+      dependencies: ['setup']
+    },
 
-        {
-            name: 'chromium',
-            use: {
-                ...devices['Desktop Chrome'],
-                storageState: 'playwright/.auth/user.json'
-            },
-            dependencies: ['setup']
-        },
+    {
+      name: 'firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+        storageState: 'playwright/.auth/user.json'
+      },
+      dependencies: ['setup']
+    },
 
-        {
-            name: 'firefox',
-            use: {
-                ...devices['Desktop Firefox'],
-                storageState: 'playwright/.auth/user.json'
-            },
-            dependencies: ['setup']
-        },
-
-        {
-            name: 'webkit',
-            use: {
-                ...devices['Desktop Safari'],
-                storageState: 'playwright/.auth/user.json'
-            },
-            dependencies: ['setup']
-        }
-    ]
+    {
+      name: 'webkit',
+      use: {
+        ...devices['Desktop Safari'],
+        storageState: 'playwright/.auth/user.json'
+      },
+      dependencies: ['setup']
+    }
+  ]
 });
--- e2e-tests/tests/example.spec.ts
+++ e2e-tests/tests/example.spec.ts
@@ -2,16 +2,16 @@ import {test, expect} from '@playwright/test';
 import {TARGET_HOST} from '../playwright.config';
 
 test('Should setup server', async ({page}) => {
-    await page.goto(TARGET_HOST);
-    await page.getByText(/^2 GB/).click();
-    await page.getByText('Next').click();
-    await page.getByText('Next').click();
-    await page.getByLabel('Generate a New World').click();
-    await page.getByText('Next').click();
-    await page.getByLabel('World Name').fill(`e2e-test-${Date.now()}`);
-    await page.getByText('Next').click();
-    await page.getByText('Next').click();
-    await page.getByText('Start').click();
+  await page.goto(TARGET_HOST);
+  await page.getByText(/^2 GB/).click();
+  await page.getByText('Next').click();
+  await page.getByText('Next').click();
+  await page.getByLabel('Generate a New World').click();
+  await page.getByText('Next').click();
+  await page.getByLabel('World Name').fill(`e2e-test-${Date.now()}`);
+  await page.getByText('Next').click();
+  await page.getByText('Next').click();
+  await page.getByText('Start').click();
 
-    await expect(page.getByText('Stop')).toBeVisible();
+  await expect(page.getByText('Stop')).toBeVisible();
 });
