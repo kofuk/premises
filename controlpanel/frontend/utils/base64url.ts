206e0f27f1c09256d97ab70ca522b801be0a7c12
Koki Fukuda <ko.fu.dev@gmail.com>
2023-10-15 21:23:07 +0900

    Introduce jest

--- controlpanel/frontend/utils/base64url.ts
+++ controlpanel/frontend/utils/base64url.ts
@@ -1,9 +1,9 @@
 export const decodeBuffer = (value: string): Uint8Array => {
-  const stdEncoding = value.replaceAll('-', '+').replaceAll('_', '/');
+  const stdEncoding = value.replace(/-/g, '+').replace(/_/g, '/');
   return Uint8Array.from(atob(stdEncoding), (c) => c.charCodeAt(0));
 };
 
 export const encodeBuffer = (value: ArrayBuffer): string => {
   const stdEncoding = btoa(String.fromCharCode.apply(null, new Uint8Array(value) as unknown as number[]));
-  return stdEncoding.replaceAll('+', '-').replaceAll('/', '_').replaceAll('=', '');
+  return stdEncoding.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+/g, '');
 };
