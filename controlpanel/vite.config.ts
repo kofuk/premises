6d9eacf42367e25cce732efa8139bba74633654a
Koki Fukuda <ko.fu.dev@gmail.com>
2023-10-15 13:44:40 +0900

    Lint

--- controlpanel/vite.config.ts
+++ controlpanel/vite.config.ts
@@ -1,6 +1,5 @@
-import path = require('path');
-import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
+import {defineConfig} from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
