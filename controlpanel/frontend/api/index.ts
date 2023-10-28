0b2251a94d70ae69637a240d686a0becaf3c3af2
Koki Fukuda <ko.fu.dev@gmail.com>
2023-10-28 19:42:06 +0900

    Use error code to show error

--- controlpanel/frontend/api/index.ts
+++ controlpanel/frontend/api/index.ts
@@ -1,34 +1,39 @@
+import {t} from 'i18next';
 import useSWR, {KeyedMutator} from 'swr';
 import useSWRImmutable from 'swr/immutable';
 
 import {MCVersion, Passkey, PasswordCredential, SessionData, SessionState, WorldBackup} from './entities';
 
+
 const domain = process.env.NODE_ENV === 'test' ? 'http://localhost' : '';
 
-const api =
-  <T, U>(endpoint: string, method: string = 'get') =>
-  async (body: T | undefined): Promise<U> => {
-    const options = {method};
-    if (body) {
-      options.body = JSON.stringify(body);
-    }
+const api = async <T, U>(endpoint: string, method: string = 'get', body: T | undefined) => {
+  const options = {method};
+  if (body) {
+    options.body = JSON.stringify(body);
+  }
 
-    const resp = await fetch(`${domain}${endpoint}`, options).then((resp) => resp.json());
+  const resp = await fetch(`${domain}${endpoint}`, options).then((resp) => resp.json());
 
-    if (!resp.success) {
-      throw new Error(resp.reason || resp.errorCode);
-    }
+  if (!resp.success) {
+    throw new Error(t(`error.code_${resp.errorCode}`));
+  }
 
-    return resp.data as U;
-  };
+  return resp.data as U;
+};
 
 export default api;
 
-export const login = api<PasswordCredential, SessionState>('/login', 'post');
-export const getSessionData = api<null, SessionData>('/api/session-data');
-export const getBackups = api<null, WorldBackup[]>('/api/backups');
-export const getMCVersions = api<null, MCVersion[]>('/api/mcversions');
-export const getPasskeys = api<null, Passkey[]>('/api/hardwarekey');
+const declareApi =
+  <T, U>(endpoint: string, method: string = 'get') =>
+  (body: T | undefined) =>
+    api<T, U>(endpoint, method, body);
+
+export const login = declareApi<PasswordCredential, SessionState>('/login', 'post');
+export const getSessionData = declareApi<null, SessionData>('/api/session-data');
+export const getBackups = declareApi<null, WorldBackup[]>('/api/backups');
+export const getMCVersions = declareApi<null, MCVersion[]>('/api/mcversions');
+export const getPasskeys = declareApi<null, Passkey[]>('/api/hardwarekey');
 
 export type ImmutableUseResponse<T> = {
   data: T | undefined;
