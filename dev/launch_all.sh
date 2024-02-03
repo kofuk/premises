787931c60308ad985e1a4a02ce75e805ae290eb8
Koki Fukuda <ko.fu.dev@gmail.com>
2024-02-03 23:59:30 +0900

    Fix launch_all.sh to run from project root

--- dev/launch_all.sh
+++ dev/launch_all.sh
@@ -7,9 +7,9 @@ export DOCKER_API_VERSION="$(docker version --format '{{ .Server.APIVersion }}')
 
 make -C "${project_root}/runner" deploy-dev
 
-exec tmux new-session -s "${name}" -n 'Middleware' 'docker compose up; bash' \; \
-     new-window -n 'Redis Console' 'while true; do sleep 3; docker compose exec redis redis-cli; done' \; \
-     new-window -n 'PostgreSQL Console' 'while true; do sleep 3; docker compose exec postgres psql -Upremises; done' \; \
+exec tmux new-session -s "${name}" -n 'Middleware' "cd ${project_root}/dev; docker compose up; bash" \; \
+     new-window -n 'Redis Console' "cd ${project_root}/dev; while true; do sleep 3; docker compose exec redis redis-cli; done" \; \
+     new-window -n 'PostgreSQL Console' "cd ${project_root}/dev; while true; do sleep 3; docker compose exec postgres psql -Upremises; done" \; \
      new-window -n 'ConoHa Emulator' "cd ${project_root}/ostack-fake; go run .; bash" \; \
      new-window -n 'Frontend' "cd ${project_root}/controlpanel; npm start; bash" \; \
      new-window -n 'Backend' "cd ${project_root}/controlpanel; sleep 3; go run .; bash" \;
