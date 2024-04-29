babdd04febd681c799d33d66c188960a75e1ee4a
Koki Fukuda <ko.fu.dev@gmail.com>
2024-04-29 22:41:09 +0900

    dev: Avoid mounting image failing on Docker

--- dev/fake_cloud_init.sh
+++ dev/fake_cloud_init.sh
@@ -4,6 +4,14 @@ if [ ! -e /userdata ]; then
     exit 1
 fi
 
+{
+    # XXX  Create loop[0-3] device node so that mounting image don't fail.
+    loop_devmajor=$(grep loop /proc/devices | cut -c3)
+    for i in 0 1 2 3; do
+        mknod "/dev/loop${i}" b "${loop_devmajor}" "${i}"
+    done
+}
+
 cat /userdata | base64 -d >/userdata_decoded.sh
 chmod +x /userdata_decoded.sh
 
