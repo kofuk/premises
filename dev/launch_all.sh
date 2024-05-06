1c58f4f15aac768b1cb0c3d35096d8073bf0a7e9
Koki Fukuda <ko.fu.dev@gmail.com>
2024-05-06 21:55:47 +0900

    dev: Bind host's directory to MinIO data directory

--- dev/launch_all.sh
+++ dev/launch_all.sh
@@ -6,9 +6,11 @@ cd "$(dirname "${BASH_SOURCE:-0}")/.."
 export DOCKER_API_VERSION="$(docker version --format '{{ .Server.APIVersion }}')"
 export PREMISES_PROXY_BIND='127.0.0.1:25565'
 
+mkdir /tmp/premises-world
+
 make -C ./runner deploy-dev
 
-exec tmux new-session -s "${name}" -n 'Middleware' "cd ./dev; docker compose up; bash" \; \
+exec tmux new-session -s "${name}" -n 'Middleware' "cd ./dev; docker compose -f compose.yaml -f tmpfs-minio.yaml up; bash" \; \
      new-window -n 'Redis Console' "cd ./dev; while true; do sleep 3; docker compose exec redis redis-cli; done" \; \
      new-window -n 'PostgreSQL Console' "cd ./dev; while true; do sleep 3; docker compose exec postgres psql -Upremises; done" \; \
      new-window -n 'ConoHa Emulator' "cd ./ostack-fake; go run .; bash" \; \
