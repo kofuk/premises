55f6d817fc220e25e44342c1e92a24e9f6e84792
Koki Fukuda <ko.fu.dev@gmail.com>
2024-05-19 11:17:36 +0000

    controlpanel: Improve extra server sertings page

--- controlpanel/frontend/features/launch/components/menus/game-config-extra.tsx
+++ controlpanel/frontend/features/launch/components/menus/game-config-extra.tsx
@@ -4,7 +4,23 @@ import {useTranslation} from 'react-i18next';
 import {TransitionGroup} from 'react-transition-group';
 
 import {Add as AddIcon, Delete as DeleteIcon, InfoOutlined as InfoIcon} from '@mui/icons-material';
-import {Collapse, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, ListSubheader, Stack, Typography} from '@mui/material';
+import {
+  Box,
+  Collapse,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  ListSubheader,
+  Stack,
+  Switch,
+  Typography
+} from '@mui/material';
 
 import {useLaunchConfig} from '../launch-config';
 import {MenuItem} from '../menu-container';
@@ -22,6 +38,8 @@ export const create = (): MenuItem => {
   const motd = config.motd || '';
   const inactiveTimeout = config.inactiveTimeout || -1;
 
+  const [motdDialogOpen, setMotdDialogOpen] = useState(false);
+  const [inactiveTimeoutDialogOpen, setInactiveTimeoutDialogOpen] = useState(false);
   const [serverPropsDialogOpen, setServerPropsDialogOpen] = useState(false);
 
   const addServerProps = (key: string, value: string) => {
@@ -54,10 +72,31 @@ export const create = (): MenuItem => {
     title: t('config_game_extra'),
     ui: (
       <Stack spacing={2} sx={{mt: 1}}>
-        <SaveInput fullWidth initValue={motd} label={t('server_description')} onSave={setDescription} type="text" />
-        <SaveInput fullWidth initValue={`${inactiveTimeout}`} label={t('inactive_timeout')} onSave={setTimeoutMinutes} type="number" />
+        <List>
+          <ListItem>
+            <ListItemButton disableGutters onClick={() => setMotdDialogOpen(true)}>
+              <ListItemText primary={t('server_description')} secondary={motd || <em>{t('value_not_set')}</em>} />
+            </ListItemButton>
+          </ListItem>
+          <ListItem
+            secondaryAction={
+              <Switch
+                checked={inactiveTimeout >= 0}
+                onChange={(e) => {
+                  setTimeoutMinutes(e.target.checked ? '30' : '-1');
+                }}
+              />
+            }
+          >
+            <ListItemButton disableGutters onClick={() => inactiveTimeout >= 0 && setInactiveTimeoutDialogOpen(true)}>
+              <ListItemText
+                primary={t('inactive_timeout')}
+                secondary={inactiveTimeout < 0 ? t('disabled') : t('minutes', {minutes: inactiveTimeout})}
+              />
+            </ListItemButton>
+          </ListItem>
 
-        <List subheader={<ListSubheader>{t('additional_server_properties')}</ListSubheader>}>
+          <ListSubheader>{t('additional_server_properties')}</ListSubheader>
           <TransitionGroup>
             {serverProps.map(({key, value}) => (
               <Collapse key={key}>
@@ -73,7 +112,7 @@ export const create = (): MenuItem => {
                     </IconButton>
                   }
                 >
-                  <ListItemText primary={key} secondary={value} />
+                  <ListItemText inset primary={key} secondary={value} />
                 </ListItem>
               </Collapse>
             ))}
@@ -91,6 +130,43 @@ export const create = (): MenuItem => {
             <Typography variant="body1">{t('server_properties_description')}</Typography>
           </Stack>
         </List>
+
+        <Dialog onClose={() => setMotdDialogOpen(false)} open={motdDialogOpen}>
+          <DialogTitle>{t('server_description')}</DialogTitle>
+          <DialogContent sx={{mb: 1}}>
+            <Box sx={{mt: 1}}>
+              <SaveInput
+                fullWidth
+                initValue={motd}
+                label={t('server_description')}
+                onSave={(value) => {
+                  setDescription(value);
+                  setMotdDialogOpen(false);
+                }}
+                type="text"
+              />
+            </Box>
+          </DialogContent>
+        </Dialog>
+
+        <Dialog onClose={() => setInactiveTimeoutDialogOpen(false)} open={inactiveTimeoutDialogOpen}>
+          <DialogTitle>{t('inactive_timeout')}</DialogTitle>
+          <DialogContent sx={{mb: 1}}>
+            <Box sx={{mt: 1}}>
+              <SaveInput
+                fullWidth
+                initValue={inactiveTimeout.toString()}
+                label={t('inactive_timeout_input_label')}
+                onSave={(value) => {
+                  setTimeoutMinutes(value);
+                  setInactiveTimeoutDialogOpen(false);
+                }}
+                type="number"
+              />
+            </Box>
+          </DialogContent>
+        </Dialog>
+
         <ServerPropsDialog add={addServerProps} onClose={() => setServerPropsDialogOpen(false)} open={serverPropsDialogOpen} />
       </Stack>
     ),
