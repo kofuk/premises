0fe08e7afe13dd04dc065cc06d12d18b2354193d
Koki Fukuda <ko.fu.dev@gmail.com>
2024-05-24 13:59:25 +0000

    dev: Add welcome message

--- .devcontainer/launch_all.sh
+++ .devcontainer/launch_all.sh
@@ -6,13 +6,10 @@ cd "$(dirname "${BASH_SOURCE:-0}")/.."
 export DOCKER_API_VERSION="$(docker version --format '{{ .Server.APIVersion }}')"
 export PREMISES_PROXY_BIND='127.0.0.1:25565'
 
-mkdir /tmp/premises-world
-
 make -C ./runner deploy-dev
 
-exec tmux new-session -s "${name}" \
-     new-window -n 'Redis Console' "redis-cli -h redis" \; \
-     new-window -n 'PostgreSQL Console' "psql -U premises" \; \
+exec tmux new-session -s "${name}" -n 'Redis Console' "redis-cli -h redis" \; \
+     new-window -n 'PostgreSQL Console' "psql -h postgres -U premises" \; \
      new-window -n 'ConoHa Emulator' "cd ./ostack-fake; go run .; bash" \; \
      new-window -n 'Frontend' "cd ./controlpanel; npm start; bash" \; \
      new-window -n 'Backend' "export PREMISES_MODE=web; cd ./controlpanel; sleep 3; go run .; bash" \; \
