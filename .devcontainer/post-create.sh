b8c3fcaaf33fc61b857222a968fd791e370f0027
Koki Fukuda <ko.fu.dev@gmail.com>
2024-05-25 00:53:40 +0000

    dev: Don't build unneeded binaries

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 (
-    # Create cache for go modules to speed up builds
-    cd /workspace/premises/controlpanel && go build -o /dev/null .
-    cd /workspace/premises/runner && go build -o /dev/null .
-
     # Install npm dependencies
     cd /workspace/premises/controlpanel && npm install
 )
