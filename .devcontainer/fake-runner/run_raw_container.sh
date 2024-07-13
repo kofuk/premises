3b7db362ca1647b65ce33eea4e4d5b090b77a772
Koki Fukuda <ko.fu.dev@gmail.com>
2024-07-13 09:16:11 +0000

    dev: Fix raw container

--- .devcontainer/fake-runner/run_raw_container.sh
+++ .devcontainer/fake-runner/run_raw_container.sh
@@ -2,20 +2,36 @@
 
 cd "$(dirname "${BASH_SOURCE:-0}")"
 
-docker build -t premises.kofuk.org/dev-runner-raw .
+docker build --tag premises.kofuk.org/dev-runner-raw .
 
 name="premises-runner-${RANDOM}"
 
-docker container run \
-       -d -p 127.0.0.2:25565:25565 --privileged \
-       -v /dev/null:/userdata:ro \
-       --name "${name}" \
-       --cap-add MKNOD \
-       --add-host host.docker.internal:host-gateway \
+host_nameserver=$(grep '^nameserver' /etc/resolv.conf | head -1 | cut --delimiter ' ' --fields 2)
+
+(
+    cd ../../runner
+    make deploy-dev
+)
+
+extra_flags=( )
+if [ "${1:-}" = 'manage' ]; then
+    extra_flags=(
        --label org.kofuk.premises.managed=true \
        --label org.kofuk.premises.id="$(uuidgen)" \
-       --label org.kofuk.premises.name="runner-${RANDOM}" \
+       --label org.kofuk.premises.name="runner-${RANDOM}"
+    )
+fi
+
+docker container run \
+       --detach --privileged \
+       --volume /dev/null:/userdata:ro \
+       --volume /tmp/premises:/premises-dev \
+       --name "${name}" \
+       --cap-add MKNOD \
+       --network host \
+       --dns "${host_nameserver}" \
+       "${extra_flags}" \
        premises.kofuk.org/dev-runner-raw
 
-docker exec -it "${name}" bash
+docker exec --interactive --tty "${name}" bash
 
