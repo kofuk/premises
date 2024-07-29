914832f73e66cd50fa661027ec23087753c695f8
Koki Fukuda <ko.fu.dev@gmail.com>
2024-07-29 10:44:17 +0000

    runner: Move packages

--- runner/internal/commands/sysupdate/sysupdate.go
+++ runner/internal/commands/sysupdate/sysupdate.go
@@ -1,10 +1,10 @@
-package keepsystemutd
+package sysupdate
 
 import (
 	"github.com/kofuk/premises/runner/internal/system"
 )
 
-func KeepSystemUpToDate(args []string) int {
+func Run(args []string) int {
 	system.AptGet("upgrade", "-y")
 	system.AptGet("autoremove", "-y", "--purge")
 	return 0
