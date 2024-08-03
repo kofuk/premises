910865248628ed659bc7947d5bb828f6c8b92db5
Koki Fukuda <ko.fu.dev@gmail.com>
2024-08-03 06:50:41 +0000

    controlpanel: Add cron job

--- controlpanel/internal/cron/cron.go
+++ controlpanel/internal/cron/cron.go
@@ -2,29 +2,126 @@ package cron
 
 import (
 	"context"
+	"errors"
+	"log/slog"
 	"math/rand"
 	"time"
 
 	"github.com/kofuk/premises/controlpanel/internal/config"
+	"github.com/kofuk/premises/controlpanel/internal/conoha"
 	"github.com/robfig/cron/v3"
 )
 
-type CronService struct{}
-
-func (cr *CronService) runSaveStorageJob() {
+type CronService struct {
+	conoha  *conoha.Client
+	nameTag string
 }
 
-func (cr *CronService) runCreateStorageJob() {
-}
+func NewCronService(config *config.Config) *CronService {
+	identity := conoha.Identity{
+		User:     config.ConohaUser,
+		Password: config.ConohaPassword,
+		TenantID: config.ConohaTenantID,
+	}
+	endpoints := conoha.Endpoints{
+		Identity: config.ConohaIdentityService,
+		Compute:  config.ConohaComputeService,
+		Image:    config.ConohaImageService,
+		Volume:   config.ConohaVolumeService,
+	}
+	conoha := conoha.NewClient(identity, endpoints, nil)
 
-func withDelay(fn func()) func() {
-	return func() {
-		time.Sleep(time.Duration(rand.Intn(10)) * time.Minute)
-		fn()
+	return &CronService{
+		conoha: conoha,
 	}
 }
 
-func (cr *CronService) Run(ctx context.Context, config *config.Config) error {
+func (cr *CronService) runSaveStorageJob() error {
+	images, err := cr.conoha.ListImages(context.Background())
+	if err != nil {
+		return err
+	}
+
+	for _, image := range images.Images {
+		if image.Name == cr.nameTag {
+			// If the image already exists, delete it first.
+			err := cr.conoha.DeleteImage(context.Background(), conoha.DeleteImageInput{
+				ImageID: image.ID,
+			})
+			if err != nil {
+				return err
+			}
+			break
+		}
+	}
+
+	volumes, err := cr.conoha.ListVolumes(context.Background())
+	if err != nil {
+		return err
+	}
+
+	var volume *conoha.Volume
+	for _, v := range volumes.Volumes {
+		if v.Name == cr.nameTag {
+			volume = &v
+			break
+		}
+	}
+
+	if volume == nil {
+		return errors.New("volume not found")
+	}
+
+	err = cr.conoha.SaveVolumeImage(context.Background(), conoha.SaveVolumeImageInput{
+		VolumeID:  volume.ID,
+		ImageName: cr.nameTag,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (cr *CronService) runCreateStorageJob() error {
+	images, err := cr.conoha.ListImages(context.Background())
+	if err != nil {
+		return err
+	}
+
+	var image *conoha.Image
+	for _, i := range images.Images {
+		if i.Name == cr.nameTag {
+			image = &i
+			break
+		}
+	}
+
+	if image == nil {
+		return errors.New("image not found")
+	}
+
+	_, err = cr.conoha.CreateBootVolume(context.Background(), conoha.CreateBootVolumeInput{
+		ImageID: image.ID,
+		Name:    cr.nameTag,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func withDelay(fn func() error) func() {
+	return func() {
+		time.Sleep(time.Duration(rand.Intn(10)) * time.Minute)
+		if err := fn(); err != nil {
+			slog.Error("cron job failed", slog.Any("error", err))
+		}
+	}
+}
+
+func (cr *CronService) Run(ctx context.Context) error {
 	c := cron.New(cron.WithLocation(time.UTC))
 
 	// 23:30 on Sunday (JST)
