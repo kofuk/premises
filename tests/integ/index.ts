71cd5ce2a591e90b952f8bbb7b665c0620b19d7f
Koki Fukuda <ko.fu.dev@gmail.com>
2024-11-11 13:11:30 +0000

    tests: Simplify

--- tests/integ/index.ts
+++ tests/integ/index.ts
@@ -3,14 +3,16 @@ import {
   assertNotEquals,
 } from "https://deno.land/std@0.83.0/testing/asserts.ts";
 
-import api, { login } from "../lib/api.ts";
+import api, { login } from "./lib/api.ts";
 import {
+  initConfig,
   launchExistingWorld,
   launchNewWorld,
   stopServer,
-} from "../lib/easy.ts";
-import { usingFakeMinecraftServer } from "../lib/env.ts";
-import { getState } from "../lib/mcproto.ts";
+  waitServerLaunched,
+} from "./lib/easy.ts";
+import { usingFakeMinecraftServer } from "./lib/env.ts";
+import { getState } from "./lib/mcproto.ts";
 
 console.log("Login");
 const accessToken = await login("admin", "password");
@@ -18,7 +20,16 @@ const accessToken = await login("admin", "password");
 const worldName = `test-${Date.now()}`;
 
 console.log("Launch server");
-await launchNewWorld(accessToken, worldName);
+await initConfig(accessToken, worldName);
+
+await api("PUT /api/v1/config", accessToken, {
+  serverPropOverride: {
+    "initial-enabled-packs": "vanilla,update_1_21",
+  },
+});
+await api("POST /api/v1/launch", accessToken);
+
+await waitServerLaunched(accessToken);
 
 const worldInfo = await api("GET /api/v1/worldinfo", accessToken);
 assertEquals(worldInfo["version"], "1.20.1");
@@ -28,7 +39,10 @@ let worldVersion: string | null = null;
 
 if (usingFakeMinecraftServer()) {
   const state = await getState();
-  assertEquals(state.serverState!.worldVersionPrev, "");
+  assertEquals(
+    state.serverState!.serverProps["initial-enabled-packs"],
+    "vanilla,update_1_21",
+  );
   worldVersion = state.serverState!.worldVersion as string;
 }
 
