ae82a2a9a049c0ae4cca42642472a10d9e8be70e
Koki Fukuda <ko.fu.dev@gmail.com>
2025-01-19 08:48:23 +0000

    dev: Add script to bump version

---
+++ bin/bump-version.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -eu
+
+new_version="$1"
+
+cd "$(git rev-parse --show-toplevel)"
+
+(
+    sed -i "s@^\\( \\+image: ghcr.io/kofuk/premises:\\)[0-9]\\+\\.[0-9]\\+\\.[0-9]\\+\$@\\1${new_version}@" compose.yaml
+)
+
+(
+    cd charts/premises
+
+    sed -i "s/^\\(version: \\|appVersion: \"\\)[0-9]\\+\\.[0-9]\\+\\.[0-9]\\+\\(\"\\?\\)\$/\\1${new_version}\\2/;" Chart.yaml
+    sed -i "s@^\\(image: ghcr.io/kofuk/premises:\\)[0-9]\\+\\.[0-9]\\+\\.[0-9]\\+\$@\\1${new_version}@" values.yaml
+)
+
+(
+    cd controlpanel/frontend
+
+    sed -i "s/\"version\": \"[0-9]\\+\\.[0-9]\\+\\.[0-9]\\+\",\$/\"version\": \"${new_version}\",/" package.json
+    npm install
+)
